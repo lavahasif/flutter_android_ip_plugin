046ad54a3aeb161c9aea5de31a9a10ba681e087a
lavahasif <lavahasif@gmail.com>
2021-09-18 01:02:31 +0530

    updated9

--- pigeons/NetworkResult.dart
+++ pigeons/NetworkResult.dart
@@ -1,45 +1,46 @@
-// flutter pub run pigeon --i nput pigeons\NetworkResult.dart --dart_out lib\pigeon.dart  --java_out android/src/main/kotlin/com/shersoft/android_ip/Pigeon.java --java_package "com .shersoft.android_ip"
-
-// // // // import 'package:pigeon/pigeon_lib.dart';
-// // //
-class NetworkResult {
-  String? wifi;
-  String? wifi_tether;
-  String? wifiboth;
-  String? privates;
-  String? cellular;
-  String? Usb;
-  String? Bluethooth;
-  String? WifiName;
-  String? all_interface;
-  bool? IsWifiConnected;
-  bool? IsHotspotEnabled;
-  bool? IsWifiEnabled;
-  bool? IsLocationEnabled;
-}
-
-@HostApi()
-abstract class Api2Host {
-  @async
-  NetworkResult getResult(NetworkResult results);
-
-  @async
-  Hotspot getHotspot(Hotspot results);
-}
-
-@HostApi()
-abstract class PermissionResult2Host {
-  @async
-  PermissionResult getResult(PermissionResult results);
-}
-
-class PermissionResult {
-  String? name;
-  bool? status;
-}
-
-class Hotspot {
-  String? name;
-  String? presharedkey;
-  bool? enabled;
-}
+// // flutter pub run pigeon --i nput pigeons\NetworkResult.dart --dart_out lib\pigeon.dart  --java_out android/src/main/kotlin/com/shersoft/android_ip/Pigeon.java --java_package "com .shersoft.android_ip"
+//
+// // // // // import 'package:pigeon/pigeon_lib.dart';
+// // // //
+// class NetworkResult {
+//   String? wifi;
+//   String? wifi_tether;
+//   String? wifiboth;
+//   String? privates;
+//   String? cellular;
+//   String? Usb;
+//   String? Bluethooth;
+//   String? WifiName;
+//   String? all_interface;
+//   bool? IsWifiConnected;
+//   bool? IsHotspotEnabled;
+//   bool? IsWifiEnabled;
+//   bool? IsLocationEnabled;
+// }
+//
+// @HostApi()
+// abstract class Api2Host {
+//   @async
+//   NetworkResult getResult(NetworkResult results);
+//
+//   @async
+//   Hotspot getHotspot(Hotspot results);
+// }
+//
+// @HostApi()
+// abstract class PermissionResult2Host {
+//   @async
+//   PermissionResult getResult(PermissionResult results);
+// }
+//
+// class PermissionResult {
+//   String? name;
+//   bool? status;
+// }
+//
+// class Hotspot {
+//   String? name;
+//   String? presharedkey;
+//   bool? enabled;
+// }
+// flutter pub run pigeon --input pigeons\NetworkResult.dart --dart_out lib\pigeon.dart  --java_out android/src/main/kotlin/com/shersoft/a ndroid_ip/Pigeon.java --java_package "com.shersoft.android_ip"
